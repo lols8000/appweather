3bff3425545b544455a2a1bda35b6769a25ce639
Marcos Vinicius S. Bezerra <vinicius.8000@gmail.com>
2021-10-12 23:21:55 -0400

    Update index.js
    Correção do erro de bd

--- src/index.js
+++ src/index.js
@@ -4,22 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const Client = require('pg').Client
-const cliente = new client({
-  user: "postgres",
-  password: "159753",
-  host: "127.0.0.1",
-  port: 5432,
-  database: "ranking"
-})
-
-cliente.connect()
-cliente.query("SELECT * FROM citiesPosition")
-.then(results => {
-  const resultado = results.rows
-  console.table(resultado)
-})
-
 .finally(() => cliente.end())
 ReactDOM.render(
   <React.StrictMode>
