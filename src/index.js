ecb1058b88530cffaa40c69c4aeaacfb52da3e60
Marcos Vinicius S. Bezerra <vinicius.8000@gmail.com>
2021-10-12 23:24:15 -0400

    Update index.js
    Correção do erro

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-.finally(() => cliente.end())
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 reportWebVitals();
+
